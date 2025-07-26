06540f7768169642f1b755b3d46018bb6af25da8
Jay Patel <Jaynpatel18@gmail.com>
2025-07-26 14:48:35 -0400

    login adjusted

--- login.php
+++ login.php
@@ -36,6 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Computer Store</title>
     <link href="./css/bootstrap.css" rel="stylesheet">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="./css/style.css" rel="stylesheet">
 </head>
 <body>
